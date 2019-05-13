76fc061c92e8a960e6e02519bbba384fad549eb3
Andres <andres.bd@outlook.com>
2019-05-13 14:05:04 -0500

    added character and camera

---
+++ Character.cpp
@@ -0,0 +1,9 @@
+//
+//  Character.cpp
+//  proyFinal
+//
+//  Created by Andres Bustamante on 5/13/19.
+//  Copyright © 2019 Andres Bustamante. All rights reserved.
+//
+
+#include "Character.hpp"
---
+++ Character.hpp
@@ -0,0 +1,14 @@
+//
+//  Character.hpp
+//  proyFinal
+//
+//  Created by Andres Bustamante on 5/13/19.
+//  Copyright © 2019 Andres Bustamante. All rights reserved.
+//
+
+#ifndef Character_hpp
+#define Character_hpp
+
+#include <stdio.h>
+
+#endif /* Character_hpp */
