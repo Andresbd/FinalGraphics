5c0c6313e1e6d7391fa43b27be1745bb9c5e0ff0
Andres <andres.bd@outlook.com>
2019-05-17 12:17:42 -0500

    Added Car from partners

---
+++ Car.cpp
@@ -0,0 +1,9 @@
+//
+//  Car.cpp
+//  proyFinal
+//
+//  Created by Andres Bustamante on 5/17/19.
+//  Copyright © 2019 Andres Bustamante. All rights reserved.
+//
+
+#include "Car.hpp"
---
+++ Car.hpp
@@ -0,0 +1,14 @@
+//
+//  Car.hpp
+//  proyFinal
+//
+//  Created by Andres Bustamante on 5/17/19.
+//  Copyright © 2019 Andres Bustamante. All rights reserved.
+//
+
+#ifndef Car_hpp
+#define Car_hpp
+
+#include <stdio.h>
+
+#endif /* Car_hpp */
